9af8a1b81d15a30d57c1c1b0895bc9eddd3e44e0
Loic Vaillé <loic@jolimoi.com>
2019-04-17 09:56:41 +0200

    step2

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{info}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    axios
+      .get("https://cfp.devoxx.fr/api/conferences/DevoxxFR2019/speakers")
+      .then(response => (this.info = response));
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
