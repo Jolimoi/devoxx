7910fbac35c4aa94b4db208944564b8459adbd87
Loic Vaillé <loic@jolimoi.com>
2019-04-17 11:02:59 +0200

    Step 4

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = {
+    configureWebpack: {
+        plugins: [
+            new WorkboxPlugin.GenerateSW({
+                swDest: "sw.js",
+                runtimeCaching: [{
+                    urlPattern: new RegExp('^https://cfp\.devoxx\.fr/'),
+                    handler: 'staleWhileRevalidate',
+                }],
+                // Make sure new versions of the Service Worker activate immediately
+                clientsClaim: true,
+                skipWaiting: true,
+            }),
+        ]
+    }
+};
