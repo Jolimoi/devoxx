7bdab9cfe979b5ff9f0df167cfa42b4dfb52d61e
Loic Vaillé <loic@jolimoi.com>
2019-04-17 11:06:55 +0200

    Step 4 b cache img

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,12 @@ module.exports = {
                 runtimeCaching: [{
                     urlPattern: new RegExp('^https://cfp\.devoxx\.fr/'),
                     handler: 'staleWhileRevalidate',
-                }],
+                },
+                {
+                    urlPattern: new RegExp('(http(s?):)([/|.|\\w|\\s|-])*\\.(?:jpg|gif|png)'),
+                    handler: 'staleWhileRevalidate'
+                }
+                ],
                 // Make sure new versions of the Service Worker activate immediately
                 clientsClaim: true,
                 skipWaiting: true,
